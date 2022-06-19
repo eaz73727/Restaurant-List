feb28da78a39c6933978ab3e8f2bb0a811a10dd2
WIlson <eaz73727@gmail.com>
2022-06-19 15:44:14 +0800

    feat:render show & search finnished

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants })
 })
 
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant = restaurants.find(restaurant => restaurant.id.toString() === req.params.id)
+  res.render('show', { restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim()
+  const filteredRestaurants = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
+  const noFile = filteredRestaurants.length ? false : true
+  res.render('index', { restaurants:filteredRestaurants , keyword, noFile })
+})
+
 app.listen(port, () => {
   console.log(`server is now opening on http://localhost:${port}`)
 })
