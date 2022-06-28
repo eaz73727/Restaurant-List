898f29fc65987820cd9b8b2dbe36b822ec71427a
WIlson <eaz73727@gmail.com>
2022-06-28 23:54:33 +0800

    feat:add remove function & index.hbs add remove button

--- app.js
+++ app.js
@@ -85,6 +85,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`server is now opening on http://localhost:${port}`)
 })
