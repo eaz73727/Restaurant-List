f8535d56e0cc8c9e67ded44da4f34c25acc5e6ea
WIlson <eaz73727@gmail.com>
2022-08-25 20:28:53 +0800

    feat: add middleware as authenticator and modify restaurants routes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const restaurantController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const restaurants = require('./modules/restaurants')
+const { authenticator } = require('../middleware/auth.js')
 
+router.use('/restaurants', authenticator, restaurants)
 router.post('/users/login', userController.postLogin)
 router.get('/users/login', userController.loginPage)
 router.post('/users/register', userController.postRegister)
 router.get('/users/register', userController.registerPage)
-router.delete('/restaurants/:id', restaurantController.deleteRestaurant)
-router.put('/restaurants/:id', restaurantController.putRestaurant)
-router.get('/restaurants/:id/edit', restaurantController.editRestaurantPage)
-router.get('/restaurants/:id', restaurantController.detailRestaurantPage)
-router.post('/restaurants', restaurantController.postRestaurant)
-router.get('/restaurants/new', restaurantController.newRestaurantPage)
-router.get('/', restaurantController.getRestaurants)
+router.use('/', (req, res) => res.redirect('/restaurants/'))
 
 module.exports = router
---
+++ routes/modules/restaurants.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const restaurantController = require('../../controllers/restaurant-controller')
+
+router.get('/new', restaurantController.newRestaurantPage)
+router.delete('/:id', restaurantController.deleteRestaurant)
+router.put('/:id', restaurantController.putRestaurant)
+router.get('/:id/edit', restaurantController.editRestaurantPage)
+router.get('/:id', restaurantController.detailRestaurantPage)
+router.post('/', restaurantController.postRestaurant)
+router.get('/', restaurantController.getRestaurants)
+
+module.exports = router
