5dbb8dbe1db1e4efe19420ac9d5d448671ae57ff
WIlson <eaz73727@gmail.com>
2022-08-25 15:15:12 +0800

    feat: set up create & detail restaurant route and page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -5,6 +5,20 @@ const restaurantController = {
       .lean()
       .sort({ _id: 'asc' })
       .then(restaurants => res.render('index', { restaurants }))
+  },
+  newRestaurantPage: (req, res) => {
+    res.render('new')
+  },
+  postRestaurant: (req, res) => {
+    Restaurant.create(req.body)
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  },
+  detailRestaurantPage: (req, res) => {
+    const id = req.params.id
+    Restaurant.findById(id)
+      .lean()
+      .then(restaurant => res.render('detail', { restaurant }))
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const restaurantController = require('../controllers/restaurant-controller')
 
+router.get('/restaurants/:id', restaurantController.detailRestaurantPage)
+router.post('/restaurants', restaurantController.postRestaurant)
+router.get('/restaurants/new', restaurantController.newRestaurantPage)
 router.get('/', restaurantController.getRestaurants)
 
 module.exports = router
